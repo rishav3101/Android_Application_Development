192c99f143c9390a0085de26c13303a97c3ca18b
unknown <phamp9331@gmail.com>
2020-10-01 09:26:07 +0700

    Update Lab02-01

--- Linear_Relative_Layout/Lab02-01/app/src/main/java/com/example/exercise01/MainActivity.java
+++ Linear_Relative_Layout/Lab02-01/app/src/main/java/com/example/exercise01/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userName_Input, passWord_Input;
+    private TextView userName_text, passWord_text;
+    private Button LogIn;
+    private CheckBox Remember_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
